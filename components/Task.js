87feea52b7ab2912bcbab1b6e3ae3d73caaa6c31
Desire <desireirankundawilliams@gmail.com>
2023-02-02 21:47:34 +0800

    able to display info on the ui

---
+++ components/Task.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, styleSheet } from "react-native";
+
+const Task = () => {
+  return <Text>Task</Text>;
+};
+
+// const style = styleSheet.create({});
+export default Task;
